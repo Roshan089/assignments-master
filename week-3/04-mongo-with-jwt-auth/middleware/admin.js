4409690e03096227b2ebd5cb53490933192d0d44
Roshan <1js20is089@gmail.com>
2024-04-06 19:11:33 +0530

    with jwt

--- week-3/04-mongo-with-jwt-auth/middleware/admin.js
+++ week-3/04-mongo-with-jwt-auth/middleware/admin.js
@@ -2,6 +2,18 @@
 function adminMiddleware(req, res, next) {
     // Implement admin auth logic
     // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtTOken = words[1];
+    const decodedValue = jwt.verify(jwtTOken, secret);
+    if (decodedValue.username) {
+        next();
+    }
+    else {
+        res.status(403).json({
+            mes:"you r not authencated"
+        })
+    }
 }
 
 module.exports = adminMiddleware;
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,19 @@
-function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+// Middleware for handling auth
+function adminMiddleware(req, res, next) {
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtTOken = words[1];
+    const decodedValue = jwt.verify(jwtTOken, secret);
+    if (decodedValue.username) {
+        next();
+    }
+    else {
+        res.status(403).json({
+            mes:"you r not authencated"
+        })
+    }
 }
 
-module.exports = userMiddleware;
+module.exports = adminMiddleware;
